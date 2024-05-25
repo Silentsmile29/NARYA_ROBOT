6d32d836c97a5f44e54ca030fa4f59a1b95f3d2e
Heart Beart Music <168265371+Heartbeat-xd@users.noreply.github.com>
2024-05-25 10:43:19 +0530

    Update wishcute.py

--- ANNIEMUSIC/plugins/Kishu/wishcute.py
+++ ANNIEMUSIC/plugins/Kishu/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from ANNIEMUSIC import app 
 
-SUPPORT_CHAT = "BWANDARLOK"
+SUPPORT_CHAT = "Y_K_B_M"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
