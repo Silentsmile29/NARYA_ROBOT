57902c37ab1fa9dfce15e0d9c489cdde80e7ec04
Silentsmile29 <159234561+Silentsmile29@users.noreply.github.com>
2024-07-29 15:11:48 +0530

    Update ranking.py

--- ANNIEMUSIC/plugins/tools/ranking.py
+++ ANNIEMUSIC/plugins/tools/ranking.py
@@ -16,7 +16,7 @@ user_data = {}
 
 today = {}
 
-pic = "https://telegra.ph/file/2c6d1a6f78eba6199933a.jpg"
+pic = "https://graph.org/file/dddce87de4e764c28f6ff.jpg"
 
 
 # ------------------- watcher ----------------------- #
