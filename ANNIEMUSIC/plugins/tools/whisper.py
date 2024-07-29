c82b6035fae8ec1e1aa703bee0289d6f30c5ade6
Silentsmile29 <159234561+Silentsmile29@users.noreply.github.com>
2024-07-29 15:16:16 +0530

    Update whisper.py

--- ANNIEMUSIC/plugins/tools/whisper.py
+++ ANNIEMUSIC/plugins/tools/whisper.py
@@ -20,7 +20,7 @@ async def _whisper(_, inline_query):
                 title="⦿ Wʜɪsᴘᴇʀ ⦿",
                 description=f"@{BOT_USERNAME} [ USERNAME | ID ] [ TEXT ]",
                 input_message_content=InputTextMessageContent(f"⦿ Usᴀɢᴇ:\n\n@{BOT_USERNAME} [ USERNAME | ID ] [ TEXT ]"),
-                thumb_url="https://telegra.ph/file/2c6d1a6f78eba6199933a.jpg",
+                thumb_url="https://graph.org/file/dd77a888a78c123e1ae10.jpg",
                 reply_markup=switch_btn
             )
         ]
@@ -38,14 +38,14 @@ async def _whisper(_, inline_query):
                     title="⦿ Wʜɪsᴘᴇʀ ⦿",
                     description=f"Sᴇɴᴅ A Wʜɪsᴘᴇʀ Tᴏ {user.first_name}!",
                     input_message_content=InputTextMessageContent(f"⦿ Yᴏᴜ Aʀᴇ Sᴇɴᴅɪɴɢ A Wʜɪsᴘᴇʀ Tᴏ {user.first_name}.\n\nTʏᴘᴇ Uʀ Mᴇssᴀɢᴇ/Sᴇɴᴛᴇɴᴄᴇ."),
-                    thumb_url="https://telegra.ph/file/2c6d1a6f78eba6199933a.jpg",
+                    thumb_url="https://graph.org/file/dd77a888a78c123e1ae10.jpg",
                     reply_markup=whisper_btn
                 ),
                 InlineQueryResultArticle(
                     title="➤ Oɴᴇ-Tɪᴍᴇ Wʜɪsᴘᴇʀ",
                     description=f"Sᴇɴᴅ A Oɴᴇ-Tɪᴍᴇ Wʜɪsᴘᴇʀ Tᴏ {user.first_name}!",
                     input_message_content=InputTextMessageContent(f"☞ Yᴏᴜ Aʀᴇ Sᴇɴᴅɪɴɢ A Oɴᴇ-Tɪᴍᴇ Wʜɪsᴘᴇʀ Tᴏ {user.first_name}.\n\nTʏᴘᴇ Uʀ Mᴇssᴀɢᴇ/Sᴇɴᴇᴛᴇɴᴄᴇ."),
-                    thumb_url="https://telegra.ph/file/2c6d1a6f78eba6199933a.jpg",
+                    thumb_url="https://graph.org/file/dd77a888a78c123e1ae10.jpg",
                     reply_markup=one_time_whisper_btn
                 )
             ]
@@ -102,8 +102,8 @@ async def in_help():
     answers = [
         InlineQueryResultArticle(
             title="⦿ Whisper ⦿",
-            description=f"@Annie_X_music_bot [USERNAME | ID] [TEXT]",
-            input_message_content=InputTextMessageContent(f"**❍ Usage:**\n\n@Annie_X_music_bot (Target Username or ID) (Your Message).\n\n**Example:**\n@Annie_X_music_bot @username I Wanna Phuck You"),
+            description=f"@Eva_Ro_bot [USERNAME | ID] [TEXT]",
+            input_message_content=InputTextMessageContent(f"**❍ Usage:**\n\n@Eva_Ro_bot (Target Username or ID) (Your Message).\n\n**Example:**\n@Eva_Ro_bot @username I Wanna Phuck You"),
             thumb_url="https://telegra.ph/file/2c6d1a6f78eba6199933a.jpg",
             reply_markup=switch_btn
         )
